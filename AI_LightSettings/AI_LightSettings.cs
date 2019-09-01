777f1ab6c1b869600658ae567626b75439c3be45
Mantas <syenergyy@gmail.com>
2019-09-01 09:33:52 +0300

    Create AI_LightSettings.cs

---
+++ AI_LightSettings/AI_LightSettings.cs
@@ -0,0 +1,45 @@
+using BepInEx;
+using BepInEx.Configuration;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AI_LightSettings {
+    [BepInPlugin(nameof(AI_LightSettings), nameof(AI_LightSettings), "1.0")]
+    public class AI_LightSettings : BaseUnityPlugin {
+        
+        private Light backLight;
+        public static ConfigWrapper<float> backLightIntensity { get; private set; }
+
+        void SceneChanged(Scene oldScene, Scene newScene) {
+            GameObject backLightObj = GameObject.Find("Directional Light Back");
+            
+            if (backLightObj != null) {
+                Light lightComp = backLightObj.GetComponent<Light>();
+
+                if (lightComp != null) {
+                    backLight = lightComp;
+                }
+            }
+        }
+
+        void Awake() {
+            backLightIntensity = Config.GetSetting("BackLight", "Intensity", 1f, new ConfigDescription("Intensity of the Backlight.", new AcceptableValueRange<float>(0f, 1f)));
+        }
+        
+        void Update() {
+            if (backLight != null) {
+                backLight.intensity = backLightIntensity.Value;
+            }
+        }
+
+        void OnEnable() {
+            SceneManager.activeSceneChanged += SceneChanged;
+        }
+
+        void OnDisable() {
+            SceneManager.activeSceneChanged -= SceneChanged;
+        }
+        
+    }
+}
