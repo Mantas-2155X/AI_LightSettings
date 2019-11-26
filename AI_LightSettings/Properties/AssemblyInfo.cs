10412f86d5f47845e33ec1a181e01a691649300a
Mantas <syenergyy@gmail.com>
2019-11-26 12:09:08 +0200

    Add version, change copyright strings

--- AI_LightSettings/Properties/AssemblyInfo.cs
+++ AI_LightSettings/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AI_LightSettings")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
 [assembly: AssemblyProduct("AI_LightSettings")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Mantas-2155X aka 2155X Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion(AI_LightSettings.AI_LightSettings.VERSION)]
 [assembly: AssemblyFileVersion("1.0.0.0")]
